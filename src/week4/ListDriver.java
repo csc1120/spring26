931b52c8f08f5fb48bcc1b72abc32f8e93f6552c
sean <jones@msoe.edu>
2026-02-10 16:03:22 -0600

    ArrayList - Method stubs and class header only

---
+++ src/week4/ListDriver.java
@@ -0,0 +1,17 @@
+/*
+ * Course: CSC-1120
+ * Assignment name
+ * File name
+ * Name: Sean Jones
+ * Last Updated:
+ */
+package week4;
+
+import java.util.ArrayList;
+
+public class ListDriver {
+    static void main() {
+        // Parameter can be any Object, will replace the generic <E>
+        ArrayList<Integer> words = new ArrayList<>();
+    }
+}
---
+++ src/week4/SJArrayList.java
@@ -0,0 +1,140 @@
+/*
+ * Course: CSC-1120
+ * Assignment name
+ * File name
+ * Name: Sean Jones
+ * Last Updated:
+ */
+package week4;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * A simple ArrayList implementation
+ * @param <E> The element type stored in the ArrayList. This will be erased
+ *           and replaced with a concrete data type when the List is
+ *           initialized with the Constructor.
+ */
+public class SJArrayList<E> implements List<E> {
+    // default starting size
+    // E array
+    // size variable
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return List.of();
+    }
+}
