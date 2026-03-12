3da69c0ecbfd04e20a4ef2c84cdcf09ba5933d7e
sean <jones@msoe.edu>
2026-03-12 15:49:00 -0500

    BinarySearch Part 2

--- src/week8/BinarySearch.java
+++ src/week8/BinarySearch.java
@@ -13,8 +13,9 @@ package week8;
 public class BinarySearch {
     static void main() {
         final Integer[] arr = {1, 4, 7, 9, 11, 14, 17, 21, 25};
-        System.out.println(binarySearch(arr, 14));
-        System.out.println(binarySearch(arr, 15));
+        final int[] testValues = {14, 15};
+        System.out.println(binarySearch(arr, testValues[0]));
+        System.out.println(binarySearch(arr, testValues[1]));
     }
     @SuppressWarnings({"rawtypes", "unchecked"})
     private static int binarySearch(Comparable[] arr, Comparable target) {
@@ -36,5 +37,22 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    private static int binarySearch(Comparable[] arr, int target, int start, int end) {
+        // base case
+        if(start > end) {
+            return -1;
+        }
+        int middle = (start + end) / 2;
+        if(arr[middle].compareTo(target) == 0) {
+            return middle;
+        }
+        // target is less than middle
+        if(arr[middle].compareTo(target) < 0) {
+            return binarySearch(arr, target, start, middle - 1);
+        } else {
+            return binarySearch(arr, target, middle + 1, end);
+        }
     }
 }
